2686f0a6b5ccba2849b5e57c9e788b96e3534610
Kate Lotareva <k.v.lotareva@gmail.com>
2025-04-20 12:26:22 +0300

    test 2

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print("Hello world!")
+    print("from visual studio code")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
