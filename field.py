40785250f9a8de864376e3af503e4590c93bff75
Kate Lotareva <k.v.lotareva@gmail.com>
2025-05-03 22:31:13 +0300

    расчет расстояния между гексами

--- field.py
+++ field.py
@@ -1,5 +1,6 @@
 class Hex:
     def __init__(self, coords, zone, animals, buildings):
+        """ клетка поля """
         self.coords = coords
         self.zone = zone
         self.animals = animals
@@ -15,6 +16,17 @@ class Field:
 
         for coords in hex.keys():
             self.hex[coords] = Hex(coords, *hex[coords])
+
+        """ self.hex - словарь из объектов класса Hex в формате: {(x, y): Hex((x, y), zone, animals, building)}"""
+
+    def calculate_distance(self, coords_1, coords_2: list):
+        x1 = coords_1[0]
+        y1 = coords_1[1]
+        x2 = coords_2[0]
+        y2 = coords_2[1]
+        z1 = -x1 -y1
+        z2 = -x2 -y2
+        return((abs(x1 - x2) + abs(y1 - y2) + abs(z1 - z2))/2)
 
     def add_circles(self):
 
